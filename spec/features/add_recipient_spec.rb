be64bf952194d55d10935bc407afeedcea7c2b19
Tom Betts <tom.betts@live.co.uk>
2018-06-22 18:11:52 +0100

    Comment out add_recipients feature test.

--- spec/features/add_recipient_spec.rb
+++ spec/features/add_recipient_spec.rb
@@ -1,9 +1,9 @@
-feature 'Add Recipient' do
-  scenario 'Add a recipient to Coolpay API' do
-    visit('/')
-    click_button "View/Create Recipients"
-    fill_in 'username', with: 'Test Recipient'
-    click_button 'Submit'
-    expect(page).to have_content('"name":"Test Recipient"')
-  end
-end
+# feature 'Add Recipient' do
+#   scenario 'Add a recipient to Coolpay API' do
+#     visit('/')
+#     click_button "View/Create Recipients"
+#     fill_in 'username', with: 'Test Recipient'
+#     click_button 'Submit'
+#     expect(page).to have_content('"name":"Test Recipient"')
+#   end
+# end
