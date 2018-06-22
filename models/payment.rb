d4d50fbc4462d9df93290d87523cb7893fa9ef82
Tom Betts <tom.betts@live.co.uk>
2018-06-22 18:39:00 +0100

    Add create_payment and initialize methods to Payment class.

--- models/payment.rb
+++ models/payment.rb
@@ -1,7 +1,29 @@
 class Payment
+  attr_reader :amount, :currency, :recipient_id
+
+  def initialize(amount, currency, recipient_id)
+    @amount = amount
+    @currency = currency
+    @recipient_id = recipient_id
+  end
+  
   def self.show_all_payments(service = CoolpayService)
     service.authenticate
     headers = service.authorization_headers
     response = RestClient.get('https://coolpay.herokuapp.com/api/payments', headers)
+  end
+
+  def self.create_payment(amount, currency, recipient_id, service = CoolpayService)
+    service.authenticate
+    new_payment = Payment.new(amount, currency, recipient_id)
+    values = {
+      "payment": {
+        "amount": new_payment.amount,
+        "currency": "#{new_payment.currency}",
+        "recipient_id": "#{new_payment.recipient_id}"
+      }
+    }
+    headers = service.authorization_headers
+    response = RestClient.post('https://coolpay.herokuapp.com/api/payments', values, headers)
   end
 end
--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -12,4 +12,19 @@ describe Payment do
       expect(payments).to include('"payments": [{"status": "paid", "amount": 10, "currency": "GBP", "recipient_id": "a", "id": "b" }]')
     end
   end
+
+  describe '#self.create_payment' do
+    before do
+      response_new_payment = '{ "payments": [{"status": "paid", "amount": 10, "currency": "GBP", "recipient_id": "a", "id": "b" }] }'
+      stub_request(:post, 'https://coolpay.herokuapp.com/api/payments').to_return(:body => response_new_payment, :status => 200, :headers => {})
+      response_payments = '{ "payments": [{"status": "paid", "amount": 10, "currency": "GBP", "recipient_id": "a", "id": "b" }] }'
+      stub_request(:get, 'https://coolpay.herokuapp.com/api/payments').to_return(:body => response_payments, :status => 200, :headers => {})
+    end
+
+    it 'creates a payment' do
+      Payment.create_payment(10, "GBP", "id")
+      payments = Payment.show_all_payments
+      expect(payments).to include('"payments": [{"status": "paid", "amount": 10, "currency": "GBP", "recipient_id": "a", "id": "b" }]')
+    end
+  end
 end
