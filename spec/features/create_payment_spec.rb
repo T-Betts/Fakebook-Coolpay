6c082ce7925ff73a5df63fb0320e8936299e0c0b
Tom Betts <tom.betts@live.co.uk>
2018-06-22 18:23:43 +0100

    Write feature test for creating payments.

---
+++ spec/features/create_payment_spec.rb
@@ -0,0 +1,11 @@
+feature 'Add Recipient' do
+  scenario 'Add a recipient to Coolpay API' do
+    visit('/')
+    click_button "View/Create Payments"
+    fill_in 'amount', with: 10
+    fill_in 'currency', with: 'GBP'
+    fill_in 'recipient_id', 'ABC'
+    click_button 'Submit'
+    expect(page).to have_content('"recipient_id":"ABC"')
+  end
+end
