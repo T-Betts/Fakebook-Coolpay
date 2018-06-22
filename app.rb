347a38b032adc91dabebdb74672581dcbcf0cefc
Tom Betts <tom.betts@live.co.uk>
2018-06-22 16:07:27 +0100

    Add necessary views and update app.rb guided by error messages from add_recipients feature test

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require 'sinatra/base'
 
 class FakebookCoolpay < Sinatra::Base
   get '/' do
-    "Hello World"
+    erb(:index)
+  end
+
+  get '/recipients' do
+    erb(:'recipients/index')
+  end
+
+  post '/recipients' do
+    
   end
 
   run! if app_file == $PROGRAM_NAME
