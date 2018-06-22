aaa60967a055a9977ca510b44e570aa6d772d19a
Tom Betts <tom.betts@live.co.uk>
2018-06-22 18:50:50 +0100

    Fix syntax error in feature test.

--- spec/features/check_payment_status_spec.rb
+++ spec/features/check_payment_status_spec.rb
@@ -2,7 +2,7 @@ feature 'Check Payment Status' do
   scenario 'Check the status of a payment' do
     visit('/')
     click_button "View/Create Payments"
-    fill_in 'payment_id', with 'XYZ'
+    fill_in 'payment_id', with: 'XYZ'
     click_button 'Submit'
     expect(page).to have_content('Payment Status:')
   end
